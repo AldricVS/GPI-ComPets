85515ccbff00f2ad68984398484d09b99d2be127
Maxence <maxence.hennekein@orange.fr>
2021-01-15 15:29:00 +0100

    commit modif QA

--- src/classes_tests_commits/ClassMoe2.java
+++ src/classes_tests_commits/ClassMoe2.java
@@ -1,9 +1,10 @@
 package classes_tests_commits;
 /**
  *  * @author Virayie Nathan
- *
+ * @author Maxence
  */
 
 public class ClassMoe2 {
 	private static final String AUTHOR = "VIRAYIE Nathan";
+	private String a_supprimer = "Supprime cetee ligne STP";
 }
