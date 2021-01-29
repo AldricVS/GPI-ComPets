6da88a5aaebc973917a3b426699894356a8fdf0d
Nathan Virayie <nathan.virayie@icloud.com>
2021-01-29 15:01:35 +0100

    implementation des classes concernant le chien

--- src/prototype/engine/Dog.java
+++ src/prototype/engine/Dog.java
@@ -1,5 +1,29 @@
 package prototype.engine;
 
 public class Dog {
+	private int coordX;
+	private int coordY;
+	
+	public Dog(int coordX, int coordY) {
+		this.coordX = coordX;
+		this.coordY = coordY;
+	}
 
+	public int getCoordX() {
+		return coordX;
+	}
+
+	public void setCoordX(int coordX) {
+		this.coordX = coordX;
+	}
+
+	public int getCoordY() {
+		return coordY;
+	}
+
+	public void setCoordY(int coordY) {
+		this.coordY = coordY;
+	}
+	
+	
 }
---
+++ src/prototype/engine/DogManager.java
@@ -0,0 +1,11 @@
+package prototype.engine;
+
+public class DogManager {
+	public static void punish(Dog dog, int coordX, int coordY) {
+		//
+	}
+	
+	public static void reward(Dog dog, int coordX, int coordY) {
+		//
+	}
+}
