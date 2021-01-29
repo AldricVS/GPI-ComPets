bf833e7925a7be4662eaf667da4d68045e16c345
Nathan Virayie <nathan.virayie@icloud.com>
2021-01-29 15:04:35 +0100

    correction de la classe dogmanager

--- src/prototype/engine/DogManager.java
+++ src/prototype/engine/DogManager.java
@@ -1,11 +1,21 @@
 package prototype.engine;
 
 public class DogManager {
-	public static void punish(Dog dog, int coordX, int coordY) {
+	private Dog dog;
+	
+	
+	public DogManager(Dog dog) {
+		this.dog = dog;
+	}
+
+	/**
+	 * x et y coordonnée de la case à laquelle le chien a été puni ou récompenser
+	 */
+	public void punish(int coordX, int coordY) {
 		//
 	}
 	
-	public static void reward(Dog dog, int coordX, int coordY) {
+	public void reward(int coordX, int coordY) {
 		//
 	}
 }
