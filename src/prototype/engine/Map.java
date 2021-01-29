aba5552188744d8047185ea4e1565355ba1c2f7c
Nathan Virayie <nathan.virayie@icloud.com>
2021-01-29 14:51:07 +0100

    implementation de la carte

--- src/prototype/engine/Map.java
+++ src/prototype/engine/Map.java
@@ -1,5 +1,30 @@
 package prototype.engine;
 
-public class Map {
 
+public class Map {
+	private int largeur;
+	private int longueur;
+	private int [][]map;
+	
+	public Map(int largeur, int longueur) {
+		map = new int [largeur][longueur];
+		this.largeur = largeur;
+		this.longueur = longueur;
+	}
+
+	public int getLargeur() {
+		return largeur;
+	}
+
+	public int getLongueur() {
+		return longueur;
+	}
+
+	public int[][] getMap() {
+		return map;
+	}
+	
+	
+	
+	
 }
