123dac08f2c5d7ea77e38116bd6f5fcfbf04270c
Maxence <maxence.hennekein@orange.fr>
2021-02-05 17:22:32 +0100

    map généré statiquement

--- src/compets/engine/data/map/Map.java
+++ src/compets/engine/data/map/Map.java
@@ -1,5 +1,8 @@
 package compets.engine.data.map;
 
+import compets.engine.data.map.item.BadItem;
+import compets.engine.data.map.item.GoodItem;
+
 /**
  * @author Maxence
  */
@@ -18,6 +21,26 @@ public class Map {
 	public Map() {
 		this(15,  15);
 		// TODO créer une carte complète
+		for (int row = 0; row < rowCount; row++) {
+			for (int column = 0; column < columnCount; column++) {
+				switch ((row + column)%5) {
+				default:
+				case 1:
+				case 2:
+					map[row][column] = new EmptyBox(new Position(row, column));
+					break;
+				case 3:
+					map[row][column] = new Wall(new Position(row, column));
+					break;
+				case 4:
+					map[row][column] = new GoodItem(new Position(row, column));
+					break;
+				case 5:
+					map[row][column] = new BadItem(new Position(row, column));
+					break;
+				}
+			}
+		}
 	}
 
 	public Box[][] getMap() {
