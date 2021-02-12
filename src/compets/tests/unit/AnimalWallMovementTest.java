bbe2fd7dcb9fd911cdb615d61c8632e5514dfa49
Maxence <maxence.hennekein@orange.fr>
2021-02-12 15:43:47 +0100

    création de la test suite avec :
    - vérification du déplacement avec des murs
    - interaction avec un bon objet
    - interaction avec un mauvais objet

---
+++ src/compets/tests/unit/AnimalWallMovementTest.java
@@ -0,0 +1,52 @@
+package compets.tests.unit;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import compets.engine.data.animal.Animal;
+import compets.engine.data.map.EmptyBox;
+import compets.engine.data.map.Map;
+import compets.engine.data.map.Position;
+import compets.engine.data.map.Wall;
+import compets.engine.process.AnimalManager;
+
+/**
+ * @author Maxence
+ */
+public class AnimalWallMovementTest {
+
+	private static Animal animal;
+	private static AnimalManager manager;
+	private static Map map;
+	private static Position startingPosition;
+
+	@BeforeClass
+	public static void init() {
+		map = new Map(2, 2);
+		map.getMap()[0][0] = new EmptyBox(new Position(0, 0));
+		map.getMap()[0][1] = new Wall(new Position(0, 1));
+		map.getMap()[1][0] = new Wall(new Position(1, 0));
+		map.getMap()[1][1] = new Wall(new Position(1, 1));
+
+		startingPosition = new Position(0, 0);
+		animal = new Animal(startingPosition);
+
+		manager = new AnimalManager(animal, map);
+	}
+
+	@Test
+	public void tryToWalk() {
+		manager.moveAnimal();
+	}
+
+	@AfterClass
+	public static void dontBeStuckInWall() {
+		assertTrue(animal.getPosition() == startingPosition);
+	}
+
+}
--- src/compets/tests/unit/MainTestSuite.java
+++ src/compets/tests/unit/MainTestSuite.java
@@ -1,5 +1,18 @@
 package compets.tests.unit;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * @author Maxence
+ */
+@RunWith(Suite.class)
+@SuiteClasses({
+	AnimalWallMovementTest.class,
+	AnimalInteractBadItem.class,
+	AnimalInteractGoodItem.class,
+})
 public class MainTestSuite {
 
 }
