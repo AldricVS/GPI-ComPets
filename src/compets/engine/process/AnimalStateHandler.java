6c0f23365347cbeb921c47d1c005e21040892622
Nathan Virayie <nathan.virayie@icloud.com>
2021-03-12 17:24:11 +0100

    ajout de commentaire et changement des priorités des conditions

--- src/compets/engine/process/AnimalStateHandler.java
+++ src/compets/engine/process/AnimalStateHandler.java
@@ -31,34 +31,42 @@ public class AnimalStateHandler {
 		int actionGaugeValue = actionGauge.getValue();
 		int healthGaugeValue = healthGauge.getValue();
 		
+		//dressage exemplaire
 		if (actionGaugeValue >= 90 && healthGaugeValue >= 95) {
 			return BehaviorStatesEnum.ANIMAL_EXEMPLARY;
 		}
 		
+		//bon dressage
 		if (actionGaugeValue >= 70) {
 			 return BehaviorStatesEnum.ANIMAL_GOOD_BEHAVIOR;
 		}
 		
-		if (actionGaugeValue <= 10) {
-			 return BehaviorStatesEnum.ANIMAL_VERY_BAD_BEHAVIOR;
-		}
-		
-		if (actionGaugeValue <= 30) {
-			 return BehaviorStatesEnum.ANIMAL_BAD_BEHAVIOR;
-		}
-		
+		//tres mauvaise santé
 		if (healthGaugeValue <= 10) {
 			return BehaviorStatesEnum.ANIMAL_VERY_BAD_HEALTH;
 		}
 		
+		//tres mauvais dressage
+		if (actionGaugeValue <= 10) {
+			 return BehaviorStatesEnum.ANIMAL_VERY_BAD_BEHAVIOR;
+		}
+		
+		//mauvaise santé
 		if (healthGaugeValue <= 30) {
 			return BehaviorStatesEnum.ANIMAL_BAD_HEALTH;
 		}
 		
+		//mauvais dressage
+		if (actionGaugeValue <= 30) {
+			 return BehaviorStatesEnum.ANIMAL_BAD_BEHAVIOR;
+		}
+		
+		//tres bonne santé
 		if(healthGaugeValue >= 90) {
 			return BehaviorStatesEnum.ANIMAL_VERY_GOOD_HEALTH;
 		}
 		
+		//animal en bonne santé
 		if(healthGaugeValue >= 70) {
 			return BehaviorStatesEnum.ANIMAL_GOOD_HEALTH;
 		}
