8735ff570de85114e3655979c6791ce2be90949a
Maxence <maxence.hennekein@orange.fr>
2021-03-12 16:03:49 +0100

    ajout d'un autre test + renommage des tests

--- src/compets/tests/unit/states/AnimalBadStatesMessageTest.java
+++ src/compets/tests/unit/states/AnimalBadStatesMessageTest.java
@@ -14,7 +14,7 @@ import compets.engine.process.AnimalStateHandler;
 /**
  * @author Maxence
  */
-public class AnimalBadBehaviorTagTest {
+public class AnimalBadStatesMessageTest {
 
 	private static Position position;
 	private static Animal animal;
@@ -30,9 +30,22 @@ public class AnimalBadBehaviorTagTest {
 	
 	@Test
 	public void messageVeryBadState() {
-		fail("test en creation");
 		animal.getBehavior().getActionGauge().setValue(Gauge.MIN_GAUGE);
 		animal.getBehavior().getHealthGauge().setValue(Gauge.MIN_GAUGE);
-		assertEquals(BehaviorStatesEnum.values()[1], handler.checkState());
+		assertEquals(BehaviorStatesEnum.ANIMAL_VERY_BAD_BEHAVIOR, handler.checkState());
+	}
+	
+	@Test
+	public void messageVeryBadHealth() {
+		animal.getBehavior().getActionGauge().setValue(Gauge.DEFAULT_GAUGE);
+		animal.getBehavior().getHealthGauge().setValue(Gauge.MIN_GAUGE);
+		assertEquals(BehaviorStatesEnum.ANIMAL_VERY_BAD_HEALTH, handler.checkState());
+	}
+	
+	@Test
+	public void messageBadBehavior() {
+		animal.getBehavior().getActionGauge().setValue(Gauge.DEFAULT_GAUGE / 2);
+		animal.getBehavior().getHealthGauge().setValue(Gauge.DEFAULT_GAUGE);
+		assertEquals(BehaviorStatesEnum.ANIMAL_BAD_BEHAVIOR, handler.checkState());
 	}
 }
