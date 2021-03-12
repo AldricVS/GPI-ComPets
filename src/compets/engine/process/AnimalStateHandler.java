44a9d1cfbac11ab86c2bae0ac7ecb86ce1ec2d07
Nathan Virayie <nathan.virayie@icloud.com>
2021-03-12 15:57:51 +0100

    implementation de la classe animalStateHandler

--- src/compets/engine/process/AnimalStateHandler.java
+++ src/compets/engine/process/AnimalStateHandler.java
@@ -1,6 +1,8 @@
 package compets.engine.process;
 
 import compets.engine.data.animal.Animal;
+import compets.engine.data.animal.Behavior;
+import compets.engine.data.animal.Gauge;
 import compets.engine.data.behavior.BehaviorStatesEnum;
 
 public class AnimalStateHandler {
@@ -10,7 +12,51 @@ public class AnimalStateHandler {
 		this.animal = animal;
 	}
 	
+	/**
+	 * Verifie dans quel état est la santé et le dressage de l'animal
+	 * 
+	 * @return l'etat dans lequel est l'animal selon l'enum {@code BehaviorStatesEnum}
+	 */
 	public BehaviorStatesEnum checkState(){
-		return null;
+		Behavior bh = animal.getBehavior();
+		Gauge actionGauge = bh.getActionGauge();
+		Gauge healthGauge = bh.getHealthGauge();
+		
+		int actionGaugeValue = actionGauge.getValue();
+		int healthGaugeValue = healthGauge.getValue();
+		
+		if (actionGaugeValue >= 90 && healthGaugeValue >= 95) {
+			return BehaviorStatesEnum.ANIMAL_EXEMPLARY;
+		}
+		
+		if (actionGaugeValue >= 80) {
+			 return BehaviorStatesEnum.ANIMAL_GOOD_BEHAVIOR;
+		}
+		
+		if (healthGaugeValue <= 10) {
+			return BehaviorStatesEnum.ANIMAL_VERY_BAD_HEALTH;
+		}
+		
+		if (healthGaugeValue <= 30) {
+			return BehaviorStatesEnum.ANIMAL_BAD_HEALTH;
+		}
+		
+		if (actionGaugeValue <= 10) {
+			 return BehaviorStatesEnum.ANIMAL_VERY_BAD_BEHAVIOR;
+		}
+		
+		if (actionGaugeValue <= 30) {
+			 return BehaviorStatesEnum.ANIMAL_BAD_BEHAVIOR;
+		}
+		
+		if(healthGaugeValue >= 90) {
+			return BehaviorStatesEnum.ANIMAL_VERY_GOOD_HEALTH;
+		}
+		
+		if(healthGaugeValue >= 70) {
+			return BehaviorStatesEnum.ANIMAL_GOOD_HEALTH;
+		}
+	
+		return BehaviorStatesEnum.NO_MSG;
 	}
 }
