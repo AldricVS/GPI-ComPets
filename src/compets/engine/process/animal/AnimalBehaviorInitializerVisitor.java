17a4e78a52973de32a0024705897e3d827ae201c
AldricVS <aldric.silvestre@outlook.fr>
2021-03-26 17:38:00 +0100

    h

--- src/compets/engine/process/animal/AnimalBehaviorInitializerVisitor.java
+++ src/compets/engine/process/animal/AnimalBehaviorInitializerVisitor.java
@@ -12,7 +12,7 @@ public class AnimalBehaviorInitializerVisitor implements AnimalVisitor<AnimalBeh
 	public AnimalBehaviorValuesRepository visit(Dog dog) {
 		AnimalBehaviorValuesRepository repository = new AnimalBehaviorValuesRepository();
 		repository.addValue(BehaviorValues.ACTION_BAD, -1);
-		repository.addValue(BehaviorValues.ACTION_GOOD, 1);
+		repository.addValue(BehaviorValues.ACTION_GOOD, 2);
 		repository.addValue(BehaviorValues.ACTION_GOOD_REWARDED, 2);
 		repository.addValue(BehaviorValues.ACTION_GOOD_PUNISHED, -2);
 		repository.addValue(BehaviorValues.ACTION_BAD_REWARDED, -2);
@@ -32,20 +32,20 @@ public class AnimalBehaviorInitializerVisitor implements AnimalVisitor<AnimalBeh
 	@Override
 	public AnimalBehaviorValuesRepository visit(Cat cat) {
 		AnimalBehaviorValuesRepository repository = new AnimalBehaviorValuesRepository();
-		repository.addValue(BehaviorValues.ACTION_BAD, -1);
+		repository.addValue(BehaviorValues.ACTION_BAD, -2);
 		repository.addValue(BehaviorValues.ACTION_GOOD, 1);
-		repository.addValue(BehaviorValues.ACTION_GOOD_REWARDED, 2);
-		repository.addValue(BehaviorValues.ACTION_GOOD_PUNISHED, -2);
-		repository.addValue(BehaviorValues.ACTION_BAD_REWARDED, -2);
-		repository.addValue(BehaviorValues.ACTION_BAD_PUNISHED, 2);
+		repository.addValue(BehaviorValues.ACTION_GOOD_REWARDED, 1);
+		repository.addValue(BehaviorValues.ACTION_GOOD_PUNISHED, -7);
+		repository.addValue(BehaviorValues.ACTION_BAD_REWARDED, -3);
+		repository.addValue(BehaviorValues.ACTION_BAD_PUNISHED, 3);
 		repository.addValue(BehaviorValues.ACTION_NEUTRAL_REWARDED, -2);
-		repository.addValue(BehaviorValues.ACTION_NEUTRAL_PUNISHED, -2);
+		repository.addValue(BehaviorValues.ACTION_NEUTRAL_PUNISHED, -8);
 		
 		repository.addValue(BehaviorValues.HEALTH_DONE_NOTHING, 1);
 		repository.addValue(BehaviorValues.HEALTH_PUNISH_FOR_NOTHING, -8);
-		repository.addValue(BehaviorValues.HEALTH_PUNISH_FOR_GOOD_ACTION, -15);
-		repository.addValue(BehaviorValues.HEALTH_REWARD_FOR_GOOD_ACTION, 3);
-		repository.addValue(BehaviorValues.HEALTH_GOOD_ACTION_NOT_REWARDED, -3);
+		repository.addValue(BehaviorValues.HEALTH_PUNISH_FOR_GOOD_ACTION, -20);
+		repository.addValue(BehaviorValues.HEALTH_REWARD_FOR_GOOD_ACTION, 2);
+		repository.addValue(BehaviorValues.HEALTH_GOOD_ACTION_NOT_REWARDED, -2);
 
 		return repository;
 	}
