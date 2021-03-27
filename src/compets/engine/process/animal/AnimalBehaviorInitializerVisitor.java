9868ac581b9049bedd72444f053121beeebb69cc
gateau-chloe <chloe.gateau@hotmail.fr>
2021-03-27 14:31:17 +0100

    proposition de réaction pour le renard

--- src/compets/engine/process/animal/AnimalBehaviorInitializerVisitor.java
+++ src/compets/engine/process/animal/AnimalBehaviorInitializerVisitor.java
@@ -54,18 +54,18 @@ public class AnimalBehaviorInitializerVisitor implements AnimalVisitor<AnimalBeh
 	public AnimalBehaviorValuesRepository visit(Fox fox) {
 		AnimalBehaviorValuesRepository repository = new AnimalBehaviorValuesRepository();
 		repository.addValue(BehaviorValues.ACTION_BAD, -2);
-		repository.addValue(BehaviorValues.ACTION_GOOD, 1);
-		repository.addValue(BehaviorValues.ACTION_GOOD_REWARDED, 1);
-		repository.addValue(BehaviorValues.ACTION_GOOD_PUNISHED, -7);
+		repository.addValue(BehaviorValues.ACTION_GOOD, 0);
+		repository.addValue(BehaviorValues.ACTION_GOOD_REWARDED, 2);
+		repository.addValue(BehaviorValues.ACTION_GOOD_PUNISHED, -6);
 		repository.addValue(BehaviorValues.ACTION_BAD_REWARDED, -3);
 		repository.addValue(BehaviorValues.ACTION_BAD_PUNISHED, 3);
-		repository.addValue(BehaviorValues.ACTION_NEUTRAL_REWARDED, -2);
+		repository.addValue(BehaviorValues.ACTION_NEUTRAL_REWARDED, -3);
 		repository.addValue(BehaviorValues.ACTION_NEUTRAL_PUNISHED, -8);
 		
 		repository.addValue(BehaviorValues.HEALTH_DONE_NOTHING, 1);
-		repository.addValue(BehaviorValues.HEALTH_PUNISH_FOR_NOTHING, -8);
-		repository.addValue(BehaviorValues.HEALTH_PUNISH_FOR_GOOD_ACTION, -20);
-		repository.addValue(BehaviorValues.HEALTH_REWARD_FOR_GOOD_ACTION, 2);
+		repository.addValue(BehaviorValues.HEALTH_PUNISH_FOR_NOTHING, -10);
+		repository.addValue(BehaviorValues.HEALTH_PUNISH_FOR_GOOD_ACTION, -25);
+		repository.addValue(BehaviorValues.HEALTH_REWARD_FOR_GOOD_ACTION, 1);
 		repository.addValue(BehaviorValues.HEALTH_GOOD_ACTION_NOT_REWARDED, -2);
 		
 		return repository;
